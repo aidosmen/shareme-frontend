c57bc5713b2a45daf91d8503a59915053d225a3c
Aidos.Omurzakov <Aidos.Omurzakov@payever.org>
2023-06-06 02:17:52 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 import Login from "./components/Login";
 import Home from "./container/Home";
 import "./App.css";
+import { fetchUser } from "./utils/fetchUser";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const tezst = "dasd";
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+    if (!user) navigate("/login");
+  });
 
   return (
     <Routes>
